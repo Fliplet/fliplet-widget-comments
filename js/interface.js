557e422542e61170dc49c564d8b52824a0cf7c11
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-02-28 12:19:37 +0100

    comments

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,16 @@
 // This function is used to generate the interface for the widget
 Fliplet.Widget.generateInterface({
-  // Define the fields that will be available in the interface
+  title: 'Comments',
   fields: [
-    // ...
+    {
+      name: 'showArrows',
+      type: 'radio',
+      label: 'Show navigation arrows',
+      options: [
+        { value: true, label: 'Mobile & Desktop' },
+        { value: false, label: 'Only Desktop' }
+      ],
+      default: true
+    }
   ]
 });
