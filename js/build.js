4b76043758b588d52858306580c051e2be5b4929
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-02-28 15:09:12 +0100

    comments

--- js/build.js
+++ js/build.js
@@ -19,12 +19,12 @@
 Fliplet.Widget.instance('comments', function(widgetData) {
   // Fliplet.Widget.initializeChildren(this.$el, this);
 
-  const selector = `[data-comments-id="${widgetData.id}"]`;
+  // const selector = `[data-comments-id="${widgetData.id}"]`;
 
   debugger;
   Fliplet().then(function() {
     new Vue({
-      el: $(selector)[0],
+      el: '#id',
       data: {
         message: 'Hello, Vue!'
       }
