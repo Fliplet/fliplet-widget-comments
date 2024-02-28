c909554fb5f0e669b62b24474595fd493b5c09f9
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-02-28 14:57:40 +0100

    comments

--- js/build.js
+++ js/build.js
@@ -1,16 +1,49 @@
-Fliplet.Widget.instance("comments", function (widgetData) {
-  const selector = `[data-comments-id="${widgetData.id}"]`;
-  debugger
-  Fliplet().then(function () {
-    new Vue({
-      el: $(selector)[0],
-      data() {
-        return {
-          containsData: "test data",
-        };
-      },
-      methods: {},
-      async mounted() {},
-    });
-  });
+// Fliplet.Widget.instance("comments", function (widgetData) {
+//   const selector = `[data-comments-id="${widgetData.id}"]`;
+//   debugger
+//   Fliplet().then(function () {
+//     new Vue({
+//       el: $(selector)[0],
+//       data() {
+//         return {
+//           containsData: "test data",
+//         };
+//       },
+//       methods: {},
+//       async mounted() {},
+//     });
+//   });
+// });
+
+Fliplet.Widget.instance({
+  name: 'comments',
+  render: {
+    template: [
+      '<div id="comments-container">',
+      '</div>'
+    ].join(''),
+    ready: async function() {
+      await Fliplet.Widget.initializeChildren(this.$el, this);
+
+      debugger;
+      Fliplet().then(function() {
+        new Vue({
+          el: '#comments-container',
+          data: {
+            message: 'Hello, Vue!'
+          }
+        });
+      });
+    },
+    views: [
+      // {
+      //   name: 'slides',
+      //   displayName: 'Slides',
+      //   placeholder:
+      //     '<div class="well text-center">Add Slide components to build your slider</div>',
+      //   allow: ['slide']
+      // }
+    ]
+  }
 });
+
