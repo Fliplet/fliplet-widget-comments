90d526da8ab00a37a8ff6934e924a06c4a15c63c
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-02-29 09:16:57 +0100

    comments

--- dist/build.js
+++ dist/build.js
@@ -93,6 +93,42 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+// Fliplet.Widget.instance("comments", function (widgetData) {
+//   const selector = `[data-comments-id="${widgetData.id}"]`;
+//   debugger
+//   Fliplet().then(function () {
+//     new Vue({
+//       el: $(selector)[0],
+//       data() {
+//         return {
+//           containsData: "test data",
+//         };
+//       },
+//       methods: {},
+//       async mounted() {},
+//     });
+//   });
+// });
+
+// Fliplet.Widget.instance({
+//   name: 'comments',
+//   render: {
+//     template: [
+//       '<div id="app-comments">',
+//       '</div>'
+//     ].join(''),
+//     ready: async function() {
+//       Fliplet().then(function() {
+//         new Vue({
+//           el: '#id',
+//           data: {
+//             message: 'Hello, Vue!'
+//           }
+//         });
+//       });
+//     }
+//   }
+
 Fliplet.Widget.instance('comments', function (widgetData) {
   // Fliplet.Widget.initializeChildren(this.$el, this);
   // const selector = `[data-comments-id="${widgetData.id}"]`;
@@ -106,13 +142,52 @@ Fliplet.Widget.instance('comments', function (widgetData) {
         message: 'Hello, Vue!',
         comments: [{
           id: 1,
-          text: 'Comment 1'
+          data: {
+            text: 'Comment 1',
+            userInitials: 'AB',
+            userFullName: 'Alicia B',
+            timestamp: '2020-01-01T00:00:00Z',
+            userAvatar: 'https://randomuser.me/api/portraits'
+          },
+          threads: []
         }, {
           id: 2,
-          text: 'Comment 2'
+          data: {
+            text: 'Comment 2',
+            userInitials: 'CD',
+            userFullName: 'Cory D',
+            timestamp: '2020-01-02T00:00:00Z',
+            userAvatar: null
+          },
+          threads: [{
+            id: 2,
+            data: {
+              text: 'Comment 2',
+              userInitials: 'CD',
+              userFullName: 'Cory D',
+              timestamp: '2020-01-02T00:00:00Z',
+              userAvatar: null
+            }
+          }]
         }, {
           id: 3,
-          text: 'Comment 3'
+          data: {
+            text: 'Comment 3',
+            userInitials: 'EF',
+            userFullName: 'Evan F',
+            timestamp: '2020-01-03T00:00:00Z',
+            userAvatar: null
+          },
+          threads: [{
+            id: 3,
+            data: {
+              text: 'Comment 3',
+              userInitials: 'EF',
+              userFullName: 'Evan F',
+              timestamp: '2020-01-03T00:00:00Z',
+              userAvatar: 'https://randomuser.me/api/portraits'
+            }
+          }]
         }]
       },
       computed: {
@@ -121,6 +196,12 @@ Fliplet.Widget.instance('comments', function (widgetData) {
         }
       },
       methods: {
+        getTimeFromTimestamp: function getTimeFromTimestamp(timestamp) {
+          return moment(timestamp).format('HH:mm:ss');
+        },
+        getDateFromTimestamp: function getDateFromTimestamp(timestamp) {
+          return moment(timestamp).format('MM/DD/YYYY');
+        },
         addComment: function addComment() {
           if (this.newComment) {
             this.comments.push({
--- js/libs/build.js
+++ js/libs/build.js
@@ -1,3 +1,40 @@
+// Fliplet.Widget.instance("comments", function (widgetData) {
+//   const selector = `[data-comments-id="${widgetData.id}"]`;
+//   debugger
+//   Fliplet().then(function () {
+//     new Vue({
+//       el: $(selector)[0],
+//       data() {
+//         return {
+//           containsData: "test data",
+//         };
+//       },
+//       methods: {},
+//       async mounted() {},
+//     });
+//   });
+// });
+
+// Fliplet.Widget.instance({
+//   name: 'comments',
+//   render: {
+//     template: [
+//       '<div id="app-comments">',
+//       '</div>'
+//     ].join(''),
+//     ready: async function() {
+//       Fliplet().then(function() {
+//         new Vue({
+//           el: '#id',
+//           data: {
+//             message: 'Hello, Vue!'
+//           }
+//         });
+//       });
+//     }
+//   }
+
+
 Fliplet.Widget.instance('comments', function(widgetData) {
   // Fliplet.Widget.initializeChildren(this.$el, this);
   // const selector = `[data-comments-id="${widgetData.id}"]`;
@@ -12,15 +49,54 @@ Fliplet.Widget.instance('comments', function(widgetData) {
         comments: [
           {
             id: 1,
-            text: 'Comment 1'
+            data: {
+              text: 'Comment 1',
+              userInitials: 'AB',
+              userFullName: 'Alicia B',
+              timestamp: '2020-01-01T00:00:00Z',
+              userAvatar: 'https://randomuser.me/api/portraits'
+            },
+            threads: []
           },
           {
             id: 2,
-            text: 'Comment 2'
+            data: {
+              text: 'Comment 2',
+              userInitials: 'CD',
+              userFullName: 'Cory D',
+              timestamp: '2020-01-02T00:00:00Z',
+              userAvatar: null
+            },
+            threads: [{
+              id: 2,
+              data: {
+                text: 'Comment 2',
+                userInitials: 'CD',
+                userFullName: 'Cory D',
+                timestamp: '2020-01-02T00:00:00Z',
+                userAvatar: null
+              }
+            }]
           },
           {
             id: 3,
-            text: 'Comment 3'
+            data: {
+              text: 'Comment 3',
+              userInitials: 'EF',
+              userFullName: 'Evan F',
+              timestamp: '2020-01-03T00:00:00Z',
+              userAvatar: null
+            },
+            threads: [{
+              id: 3,
+              data: {
+                text: 'Comment 3',
+                userInitials: 'EF',
+                userFullName: 'Evan F',
+                timestamp: '2020-01-03T00:00:00Z',
+                userAvatar: 'https://randomuser.me/api/portraits'
+              }
+            }]
           }
         ]
       },
@@ -30,6 +106,12 @@ Fliplet.Widget.instance('comments', function(widgetData) {
         }
       },
       methods: {
+        getTimeFromTimestamp(timestamp) {
+          return moment(timestamp).format('HH:mm:ss');
+        },
+        getDateFromTimestamp(timestamp) {
+          return moment(timestamp).format('MM/DD/YYYY');
+        },
         addComment: function() {
           if (this.newComment) {
             this.comments.push({
@@ -43,3 +125,4 @@ Fliplet.Widget.instance('comments', function(widgetData) {
     });
   });
 });
+
