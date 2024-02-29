0bc2588052a539f9ccc593d25a6c2080c68b9505
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-02-29 15:47:29 +0100

    comments

--- js/libs/build.js
+++ js/libs/build.js
@@ -106,14 +106,14 @@ Fliplet.Widget.instance('comments', function(widgetData) {
         }
       },
       methods: {
-        // getTimeFromTimestamp: function(timestamp) {
-        //   return timestamp;
-        //   // return moment(timestamp).format('HH:mm:ss');
-        // },
-        // getDateFromTimestamp: function(timestamp) {
-        //   return timestamp;
-        //   // return moment(timestamp).format('MM/DD/YYYY');
-        // },
+        getTimeFromTimestamp: function(timestamp) {
+          return timestamp;
+          // return moment(timestamp).format('HH:mm:ss');
+        },
+        getDateFromTimestamp: function(timestamp) {
+          return timestamp;
+          // return moment(timestamp).format('MM/DD/YYYY');
+        },
         addComment() {
           if (this.newComment) {
             this.comments.push({
